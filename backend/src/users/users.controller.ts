26188840a041c560a51d7a4d7d325ff186f94812
himalf <himal.fullel15@gmail.com>
2025-04-18 11:36:36 +0545

    feat: add controller for user service

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+  @Get(':id')
+  findOne(@Param() id: string) {
+    return this.usersService.findOne(id);
+  }
+  @Patch(':id')
+  update(@Body() updateUserDto: UpdateUserDto, @Param() id: string) {
+    return this.usersService.update(updateUserDto, id);
+  }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(id);
+  }
+}
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { User } from './schemas/user.schema';
 
 @Module({
   providers: [UsersService],
-  controllers: [UsersController]
+  controllers: [UsersController],
 })
 export class UsersModule {}

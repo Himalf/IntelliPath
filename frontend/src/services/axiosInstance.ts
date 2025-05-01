eec4d2effbf9fecf28f4c359e258ebcc3e2ed92d
himalf <himal.fullel15@gmail.com>
2025-05-01 12:39:17 +0545

    chore: change the URL

--- frontend/src/services/axiosInstance.ts
+++ frontend/src/services/axiosInstance.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "https://intellipath.onrender.com",
-  baseURL: "http://localhost:3000",
+  baseURL: "https://intellipath.onrender.com",
+  // baseURL: "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
   },
